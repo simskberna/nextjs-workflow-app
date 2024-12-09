a7b2302716302f7ec4fd136bfcbb882c7bcbd34e
simskberna <simskberna@gmail.com>
2024-12-09 23:38:06 +0300

    refactor: login page ui

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,30 +1,30 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input"> & { icon?: React.ReactNode }>(
-  ({ className, type, icon, ...props }, ref) => {
-    return (
-      <div className="relative w-full">
-        {icon && (
-          <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
-            {icon}
-          </div>
+const Input = React.forwardRef<
+  HTMLInputElement,
+  React.ComponentProps<"input"> & { icon?: React.ReactNode }
+>(({ className, type, icon, ...props }, ref) => {
+  return (
+    <div className="relative w-full">
+      {icon && (
+        <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+          {icon}
+        </div>
+      )}
+      <input
+        type={type}
+        className={cn(
+          "focus:border-none flex h-9 w-full rounded-md border border-input bg-transparent pl-10 pr-3 py-1 text-base shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          !icon && "pl-3",
+          className
         )}
-        <input
-          type={type}
-          className={cn(
-            "flex h-9 w-full rounded-md border border-input bg-transparent pl-10 pr-3 py-1 text-base shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            !icon && "pl-3",
-            className
-          )}
-          ref={ref}
-          {...props}
-        />
-      </div>
-    );
-  }
-);
+        ref={ref}
+        {...props}
+      />
+    </div>
+  );
+});
 
 Input.displayName = "Input";
 
